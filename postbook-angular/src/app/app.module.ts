fc6637ddb427e87cf25c73be225c1f5e02b37169
samuel.banico <samuel.banico@pointwest.com.ph>
2023-11-16 17:39:00 +0800

    Added navigation bar UI

--- postbook-angular/src/app/app.module.ts
+++ postbook-angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
