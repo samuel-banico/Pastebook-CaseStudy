2cebc78ed78988166a9142a7fa052465e866861c
samuel.banico <samuel.banico@pointwest.com.ph>
2023-11-16 13:36:34 +0800

    Added initial angular file

--- postbook-angular/README.md
+++ postbook-angular/README.md
@@ -1,6 +1,6 @@
-# PastebookAngular
+# PostbookAngular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.9.
 
 ## Development server
 
--- postbook-angular/src/app/app.component.spec.ts
+++ postbook-angular/src/app/app.component.spec.ts
@@ -2,11 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pastebook-angular' title`, () => {
+  it(`should have as title 'postbook-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pastebook-angular');
+    expect(app.title).toEqual('postbook-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pastebook-angular');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('postbook-angular app is running!');
   });
 });
