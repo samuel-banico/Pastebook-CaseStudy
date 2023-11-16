9c2a6808909363f8ee79a726d2022ac68ff848a5
josiah.sabariza <josiah.sabariza@pointwest.com.ph>
2023-11-16 12:27:18 +0800

    Coded Register

--- pastebook-db/pastebook-db/Models/User.cs
+++ pastebook-db/pastebook-db/Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace pastebook_db.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pastebook_db.Models
 {
     public class User
     {
@@ -12,5 +14,30 @@
         public string? MobileNumber { get; set; }
         public byte[] ProfilePicture { get; set; } = null!;
         public bool IsActive { get; set; }
+    }
+
+    public class UserRegister
+    {
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+        public string? MobileNumber { get; set; }
     }
 }
