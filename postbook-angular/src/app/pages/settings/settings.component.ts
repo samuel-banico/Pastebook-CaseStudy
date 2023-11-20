913149234291068a56a2ca124c7611f896b7b208
christian.deguzman <christian.deguzman@pointwest.com.ph>
2023-11-20 10:45:41 +0800

    fix search on nav-bar and settings

--- postbook-angular/src/app/pages/settings/settings.component.ts
+++ postbook-angular/src/app/pages/settings/settings.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class SettingsComponent {
 
+  div1:boolean=true;
+    div2:boolean=false;
+
+    showPassword = false;
+    
+
+    div1Function(){
+        this.div1=true;
+        this.div2=false;
+        
+    }
+
+    div2Function(){
+        this.div2=true;
+        this.div1=false;
+        
+    }
+
+   
 }
+
