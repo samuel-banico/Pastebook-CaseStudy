c42e08c2aabd34d6af39dc83b9b9e9e81d04e0a6
christian.deguzman <christian.deguzman@pointwest.com.ph>
2023-11-27 18:04:06 +0800

    Initialize changing colors

--- postbook-angular/src/app/components/postmodal/postmodal.component.ts
+++ postbook-angular/src/app/components/postmodal/postmodal.component.ts
@@ -1,56 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
 import Swal from 'sweetalert2';
 
 import { PostService } from '@services/post.service';
-import { SessionService } from '@services/session.service';
-import { Post } from '@models/post';
 import { UserService } from '@services/user.service';
+import { Post } from '@models/post';
 import { User } from '@models/user';
+
+// ... Existing imports ...
 
 @Component({
   selector: 'app-postmodal',
   templateUrl: './postmodal.component.html',
   styleUrls: ['./postmodal.component.css']
 })
-export class PostmodalComponent implements OnInit{
+export class PostmodalComponent {
   post: Post = new Post();
   user: User = new User();
+  selectedPrivacy: string = 'Public';
 
   constructor(
     public modalRef: MdbModalRef<PostmodalComponent>,
     private postService: PostService,
-    private userService: UserService,
-    private sessionService: SessionService
-    ) {
-      userService.getUserByToken().subscribe((response: Object)=>{
-        this.user = response;
-      })
-    }
+    private userService: UserService
+  ) {
+    userService.getUserByToken().subscribe((response: Object) => {
+      this.user = response as User;
+    });
+  }
 
   close(): void {
     const closeMessage = 'Modal closed';
-    this.modalRef.close(closeMessage)
+    this.modalRef.close(closeMessage);
   }
 
-  selectedOption = 'Public';
-  onOptionSelect(option: string) {
-    this.selectedOption = option;
+  onPrivacySelect(): void {
+    console.log('Selected privacy:', this.selectedPrivacy);
   }
 
-  ngOnInit(): void {}
-  onPost() {
-    console.log(this.post);
-    if (this.selectedOption == 'Public'){
-      this.post.isPublic = true;
-    } else {
-      this.post.isPublic = false;
-    }
+  onPost(): void {
+    console.log('Post content:', this.post.content);
+    console.log('Selected privacy:', this.selectedPrivacy);
+
+    // Add your logic to post the content with the selected privacy
+    // Example: Update the 'isPublic' property based on the selected privacy
+    this.post.isPublic = this.selectedPrivacy === 'Public';
+
     this.post.userId = this.user.id;
-    this.postService.createPost(this.post).subscribe((response: Record<string, any>)=>{
-      if(response['result']==='new_post'){
-        Swal.fire('Posted','Your status was posted','success');
+    this.postService.createPost(this.post).subscribe((response: Record<string, any>) => {
+      if (response['result'] === 'new_post') {
+        Swal.fire('Posted', 'Your status was posted', 'success');
+        this.close(); // Close the modal after posting
       }
-    })
+    });
   }
 }
