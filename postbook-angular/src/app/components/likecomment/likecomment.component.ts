08529b0821598e124135f83e7ba001fa7285aac1
christian.deguzman <christian.deguzman@pointwest.com.ph>
2023-12-04 16:52:08 +0800

    Finalize auto refresh

--- postbook-angular/src/app/components/likecomment/likecomment.component.ts
+++ postbook-angular/src/app/components/likecomment/likecomment.component.ts
@@ -9,6 +9,7 @@ import { User } from '@models/user';
 import { Album, AlbumImage, AlbumImageLike, AlbumImageComment } from '@models/album';
 import { Post, PostComment, PostLike } from '@models/post';
 import { TokenService } from '@services/token.service';
+import { SharedService } from '@services/shared.service';
 
 @Component({
   selector: 'app-likecomment',
@@ -29,7 +30,8 @@ export class LikecommentComponent implements OnChanges {
     private albumService: AlbumService,
     private sessionService: SessionService,
     private http: HttpClient,
-    private tokenService: TokenService
+    private tokenService: TokenService,
+    private sharedService: SharedService
   ){
     this.tokenService.validateToken();
     
@@ -57,63 +59,70 @@ export class LikecommentComponent implements OnChanges {
 
     let postId = this.sessionService.getPost();
     let albumImageId = this.sessionService.getAlbumImage();
-    window.location.reload();
-    if(this.post.id || postId) {
+
+    if (this.post.id || postId) {
       let postLike: PostLike = new PostLike();
       postLike.postId = this.post.id;
 
-      if(!postLike.postId) {
+      if (!postLike.postId) {
         postLike.postId = postId;
       }
-      
-      if(this.isLiked) 
-      {
-        this.postService.addLike(postLike).subscribe();
+
+      if (this.isLiked) {
+        this.postService.addLike(postLike).subscribe(() => {
+          this.sharedService.emitDataSaved(); // Emit event after the like is added
+        });
       } else {
-        this.postService.removeLike(postLike.postId!).subscribe();
+        this.postService.removeLike(postLike.postId!).subscribe(() => {
+          this.sharedService.emitDataSaved(); // Emit event after the like is removed
+        });
       }
     } else if (this.albumImage.id || albumImageId) {
-        let albumImageLike: AlbumImageLike = new AlbumImageLike();
-        albumImageLike.albumImageId = this.albumImage.id;
+      let albumImageLike: AlbumImageLike = new AlbumImageLike();
+      albumImageLike.albumImageId = this.albumImage.id;
 
-        if(!albumImageLike.albumImageId) {
-          albumImageLike.albumImageId = albumImageId;
-        }
+      if (!albumImageLike.albumImageId) {
+        albumImageLike.albumImageId = albumImageId;
+      }
 
-        if(this.isLiked) 
-        {
-          this.albumService.addLike(albumImageLike).subscribe();
-        } else {
-          this.albumService.removeLike(albumImageLike.albumImageId!).subscribe();
-        }
+      if (this.isLiked) {
+        this.albumService.addLike(albumImageLike).subscribe(() => {
+          this.sharedService.emitDataSaved(); // Emit event after the like is added
+        });
+      } else {
+        this.albumService.removeLike(albumImageLike.albumImageId!).subscribe(() => {
+          this.sharedService.emitDataSaved(); // Emit event after the like is removed
+        });
+      }
     }
   }
-
   comment : string = "";
   sendComment() {
-    if(this.post.id) {
+    if (this.post.id) {
       let postComment: PostComment = new PostComment();
       postComment.postId = this.post.id;
       postComment.comment = this.comment;
 
-      this.postService.addComment(postComment).subscribe(r => {
-        window.location.reload();
-        Swal.fire('Comment Successful', 'You have commented on the post', 'success');
+      this.postService.addComment(postComment).subscribe(() => {
+        
+        
+        this.sharedService.emitDataSaved();
+        
       });
 
-    } else if(this.albumImage.id) {
+    } else if (this.albumImage.id) {
       let albumImageComment: AlbumImageComment = new AlbumImageComment();
       albumImageComment.albumImageId = this.albumImage.id;
       albumImageComment.comment = this.comment;
 
-      this.albumService.addComment(albumImageComment).subscribe(a => {
-        Swal.fire('Comment Successful', 'You have commented on the post', 'success');
-      })
+      this.albumService.addComment(albumImageComment).subscribe(() => {
+       
+        this.sharedService.emitDataSaved();// Emit event after the comment is added
+      });
     }
 
     this.comment = "";
   }
-
   toggleLikesDropdown() {
     this.showLikesDropdown = !this.showLikesDropdown;
     this.showComments = false;
