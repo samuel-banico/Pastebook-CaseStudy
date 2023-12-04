9b76222464319e3c243c143980c542a807e31ae7
christian-deyto <147044872+christian-deyto@users.noreply.github.com>
2023-12-04 15:29:12 +0800

    Added Dark Mode feature

---
+++ postbook-angular/src/app/components/dark-mode-toggle/dark-mode-toggle.component.ts
@@ -0,0 +1,18 @@
+// dark-mode-toggle.component.ts
+
+import { Component } from '@angular/core';
+import { DarkModeService } from 'angular-dark-mode';
+
+@Component({
+  selector: 'app-dark-mode-toggle',
+  templateUrl: './dark-mode-toggle.component.html',
+  styleUrls: ['./dark-mode-toggle.component.css']
+})
+export class DarkModeToggleComponent {
+
+  constructor(private darkModeService: DarkModeService) { }
+
+  toggleDarkMode() {
+    this.darkModeService.toggle();
+  }
+}
